50bc204a35d7afd712a86c41dda881ca95d053b2
PXI_BBDL <PXI_BBDL@PXI_BBDL-PC.usc.edu>
2012-06-27 17:39:11 -0700

    (Minos) OpenGL graph shows the LoadCell signal in real-time

--- UPC_R01_grant/source/include/PxiDAQ.h
+++ UPC_R01_grant/source/include/PxiDAQ.h
@@ -2,8 +2,10 @@
 #define __EmgDAQ_h__
 
 #include	<NIDAQmx.h>
+#include	"pthread.h"
 
-//extern double g_force[2];
+extern double g_force[2];
+
 
 int32 CVICALLBACK EveryNCallback(TaskHandle taskHandleDAQmx, int32 everyNsamplesEventType, uInt32 nSamples, void *callbackData);
 int32 CVICALLBACK DoneCallback(TaskHandle taskHandleDAQmx, int32 status, void *callbackData);
